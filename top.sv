cd29fec9d3bc68d62651da085288d362553a852b
Tyler Henderson <tylermhenderson@outlook.com>
2026-06-03 17:01:45 -0700

    initial commit

---
+++ top.sv
@@ -0,0 +1,22 @@
+//top module for 5-stage microprocessor
+
+
+module top( input clk, rst);
+    
+// 1. instruction fetch
+
+
+// 2. instruction decode
+
+
+// 3. execute
+
+
+// 4. memory access
+
+
+// 5. register writeback
+
+
+
+endmodule: top
