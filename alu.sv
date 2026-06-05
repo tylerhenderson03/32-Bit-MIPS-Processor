127799af117a4e516f52991ba120a621b23642d5
Tyler Henderson <your_personal_email@example.com>
2026-06-05 08:38:32 -0700

    began ID stage, and added outline of basic ALU. havent optimized addition/multiplication logic yet

--- alu.sv
+++ alu.sv
@@ -1,3 +1,37 @@
 // Arithmetic Logic Unit
-// performs addition (CLA), subtraction, multiplication, LT/GT, EQ/NE
+// performs addition (CLA), subtraction, multiplication(?), LT/GT, EQ/NE
 
+
+module alu( input [WIDTH-1:0] in_a, in_b,
+            input [3:0] alu_opcode,
+            output zero_flag,
+            output wire [WIDTH-1:0] alu_out
+            );
+
+case(alu_opcode)
+    0000: begin // AND
+        assign alu_out = in_a & in_b; 
+    end
+    0001: begin // OR
+        assign alu_out = in_a | in_b; 
+
+    end
+    0010: begin // add
+        assign alu_out = in_a + in_b; 
+
+    end
+    0110: begin // subtract
+
+    end
+    0111: begin // set-on-less-than
+    
+    end
+    1100: begin // NOR
+    
+    end
+    1111: begin // MUL?
+    
+    end
+
+
+endmodule: alu
--- id_stage.sv
+++ id_stage.sv
@@ -1,3 +1,20 @@
 // Instruction Decode
 // register file read, sign-extension
 
+module if_stage(input [WIDTH-1:0] if_in,
+                input [XXX:0] pc_incr_in,
+                output wb_ctrl
+                output [2:0] mem_ctrl, ex_ctrl,
+                output [XXX:0] pc_incr_out,
+                output wire [32-1:0] s_extend_out,
+                output wire [4:0] rd_out, rt_out
+                );
+
+    reg [WIDTH-1:0] register_file [32-1:0]; // 32 registers, each are 32(DEPTH)-bits wide
+
+    assign s_extend_out = {16'b0, if_in[15:0]};
+    assign rd_out = if_in[15:11];
+    assign rt_out = if_in[20:16];
+
+
+endmodule: if_stage
